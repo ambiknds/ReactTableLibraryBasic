66b08733e65b85cd7f6f8241b7cecb9bff34814d
Ambition Kyndiah <53797801+ambiknds@users.noreply.github.com>
2023-03-15 10:12:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Running React
+npm install react-table
+npm install uuidv4
+https://www.youtube.com/watch?v=A9oUTEP-Q84
+
+https://github.com/machadop1407/react-table-tutorial
+add id to each row data
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import React from 'react';
+import Table from './components/Table';
+
+export default function App() {
+  return(
+    <div className='App'>
+      <Table />
+    </div>
+  )
+}
+  
